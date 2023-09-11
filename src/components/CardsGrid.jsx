fc24789e22bb8f6c611d0c42b59702054d1b9b84
zakrnem <zakrnem@zohomail.com>
2023-09-10 19:54:39 -0600

    New random flags being inserted after every page reload

--- src/components/CardsGrid.jsx
+++ src/components/CardsGrid.jsx
@@ -4,30 +4,174 @@ import { v4 as uuidv4 } from "uuid";
 import "../styles/CardsGrid.css";
 
 function CardsGrid({ gridSize, setScore, setBestScore, score, bestScore }) {
-  const [imagesArray, setImagesArray] = useState({
-    1: {
-      countryCode: "MZ",
-      countryName: "Mozambique",
-      key: uuidv4(),
-    },
-    2: {
-      countryCode: "NA",
-      countryName: "Namibia",
-      key: uuidv4(),
-    },
-    3: {
-      countryCode: "MW",
-      countryName: "Malawi",
-      key: uuidv4(),
-    },
-    4: {
-      countryCode: "NE",
-      countryName: "Niger",
-      key: uuidv4(),
-    },
-  });
+  const [imagesArray, setImagesArray] = useState({});
 
-  const buildImagesArray = (gridSize) => {};
+  /*  useEffect(() => {
+    console.log(imagesArray);
+  }, [imagesArray]); */
+
+  useEffect(() => {
+    const imagesObj = {
+      1: {
+        countryCode: "AR",
+        countryName: "Argentina",
+        key: uuidv4(),
+      },
+      2: {
+        countryCode: "BR",
+        countryName: "Brazil",
+        key: uuidv4(),
+      },
+      3: {
+        countryCode: "CA",
+        countryName: "Canada",
+        key: uuidv4(),
+      },
+      4: {
+        countryCode: "CO",
+        countryName: "Colombia",
+        key: uuidv4(),
+      },
+      5: {
+        countryCode: "DE",
+        countryName: "Germany",
+        key: uuidv4(),
+      },
+      6: {
+        countryCode: "DK",
+        countryName: "Denmark",
+        key: uuidv4(),
+      },
+      7: {
+        countryCode: "ES",
+        countryName: "Spain",
+        key: uuidv4(),
+      },
+      8: {
+        countryCode: "GB",
+        countryName: "United Kingdom",
+        key: uuidv4(),
+      },
+      9: {
+        countryCode: "FR",
+        countryName: "France",
+        key: uuidv4(),
+      },
+      10: {
+        countryCode: "IT",
+        countryName: "Italy",
+        key: uuidv4(),
+      },
+      11: {
+        countryCode: "PL",
+        countryName: "Poland",
+        key: uuidv4(),
+      },
+      12: {
+        countryCode: "SV",
+        countryName: "El Salvador",
+        key: uuidv4(),
+      },
+      13: {
+        countryCode: "US",
+        countryName: "United States",
+        key: uuidv4(),
+      },
+      14: {
+        countryCode: "TR",
+        countryName: "Turkey",
+        key: uuidv4(),
+      },
+      15: {
+        countryCode: "CN",
+        countryName: "China",
+        key: uuidv4(),
+      },
+      16: {
+        countryCode: "JP",
+        countryName: "Japan",
+        key: uuidv4(),
+      },
+      17: {
+        countryCode: "CL",
+        countryName: "Chile",
+        key: uuidv4(),
+      },
+      18: {
+        countryCode: "BO",
+        countryName: "Bolivia",
+        key: uuidv4(),
+      },
+      19: {
+        countryCode: "EG",
+        countryName: "Egypt",
+        key: uuidv4(),
+      },
+      20: {
+        countryCode: "FI",
+        countryName: "Finland",
+        key: uuidv4(),
+      },
+      21: {
+        countryCode: "MX",
+        countryName: "Mexico",
+        key: uuidv4(),
+      },
+      22: {
+        countryCode: "PE",
+        countryName: "Peru",
+        key: uuidv4(),
+      },
+      23: {
+        countryCode: "PT",
+        countryName: "Portugal",
+        key: uuidv4(),
+      },
+      24: {
+        countryCode: "RU",
+        countryName: "Russia",
+        key: uuidv4(),
+      },
+      25: {
+        countryCode: "SE",
+        countryName: "Sweden",
+        key: uuidv4(),
+      },
+      26: {
+        countryCode: "UY",
+        countryName: "Uruguay",
+        key: uuidv4(),
+      },
+    };
+
+    const buildImagesArray = () => {
+      let updatedImagesArray = { ...imagesArray };
+      let count = 0;
+      let prevRandomIndexes = [];
+
+      while (count < gridSize) {
+        count++;
+        const randomIndex = Math.floor(Math.random() * 26) + 1;
+        const currIndex = Object.keys(updatedImagesArray).length;
+        const value = imagesObj[randomIndex];
+
+        if (!prevRandomIndexes.includes(randomIndex)) {
+          prevRandomIndexes.push(randomIndex);
+
+          updatedImagesArray = {
+            ...updatedImagesArray,
+            [currIndex]: value,
+          };
+        } else {
+          count--;
+        }
+
+        setImagesArray(updatedImagesArray);
+      }
+    };
+
+    buildImagesArray();
+  }, [gridSize]);
 
   const [selectedCountries, setSelectedCountries] = useState([]);
 
@@ -47,7 +191,7 @@ function CardsGrid({ gridSize, setScore, setBestScore, score, bestScore }) {
   };
 
   useEffect(() => {
-    console.log(selectedCountries);
+    //console.log(selectedCountries);
   }, [selectedCountries]);
 
   return (
